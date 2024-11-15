b81bef89c111decca2e42b96dcde1e0e948db2fe
tummajyothi304 <jtumma449@gmail.com>
2024-11-15 12:08:36 +0530

    Add Secondfile.java and ThirdFile.java

---
+++ src/main/java/secondfile.java
@@ -0,0 +1,7 @@
+package demo.example;
+
+public class SecondFile {
+    public static void main(String[] args) {
+        System.out.println("Hello from SecondFile!");
+    }
+}
---
+++ src/main/java/thirdfile.java
@@ -0,0 +1,7 @@
+package com.example;
+
+public class ThirdFile {
+    public static void main(String[] args) {
+        System.out.println("Hello from ThirdFile!");
+    }
+}
